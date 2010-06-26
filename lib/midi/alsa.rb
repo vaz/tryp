3a5cabfb85f719e95af6d44719449e5cab3c7cdf
Vaz <allen.mat@gmail.com>
2010-06-26 00:22:59 -0400

    adding alsa and winmm implementations and I have no idea if they work

--- lib/midi/alsa.rb
+++ lib/midi/alsa.rb
@@ -1,1 +1,34 @@
-raise 'not implemented'
+# This is not tested at all.  It might work.
+
+require 'ffi'
+
+module Tryp
+  class MIDI
+    def open
+      output_ptr = FFI::MemoryPointer.new :pointer
+      C.snd_rawmidi_open nil, output_ptr, 'virtual', 0)
+      @output = output_ptr.read_pointer
+    end
+
+    def close
+      C.snd_rawmidi_close(@output)
+    end
+
+    def message(*args)
+      bytes = FFI::MemoryPointer.new(FFI.type_size(:char) * args.size)
+      bytes.write_string args.pack('C' * args.size)
+      C.snd_rawmidi_write @output, bytes, args.size
+      C.snd_rawmidi_drain @output
+    end
+
+    module C
+      extend FFI::Library
+      ffi_lib 'libasound.so'
+
+      attach_function :snd_rawmidi_open,  [:pointer, :pointer, :pointer, :int], :int
+      attach_function :snd_rawmidi_close, [:pointer],                           :int
+      attach_function :snd_rawmidi_write, [:pointer, :pointer, :int],           :int
+      attach_function :snd_rawmidi_drain, [:pointer],                           :int
+    end
+  end
+end
--- lib/midi/winmm.rb
+++ lib/midi/winmm.rb
@@ -1,1 +1,31 @@
-raise 'not implemented'
+# This is not tested at all.  It might work.
+
+require 'ffi'
+
+module Tryp
+  class MIDI
+    def open
+      device_ptr = FFI::MemoryPointer.new(FFI.type_size(:int))
+      C.midiOutOpen device_ptr, -1, 0, 0, 0)
+      @device = device_ptr.read_int
+    end
+
+    def close
+      C.midiOutClose @device
+    end
+
+    def message(one, two=0, three=0)
+      message = one + (two << 8) + (three << 16)
+      C.midiOutShortMsg @device, message
+    end
+
+    module C
+      extend FFI::Library
+      ffi_lib 'winmm'
+
+      attach_function :midiOutOpen, [:pointer] + [:int] * 4, :int
+      attach_function :midiOutClose, [:int], :int
+      attach_function :midiOutShortMsg, [:int]*2, :int
+    end
+  end
+end
